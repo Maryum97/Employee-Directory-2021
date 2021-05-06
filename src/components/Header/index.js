d425d9221fa5fa8b7c95f4900f432066790f9340
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-05-06 20:36:53 +0100

    added new components and edited styling

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+    return (
+        <header className="header">
+            <h1>Employee Tracker</h1>
+            <p>Search for an employee or sort by Name or Category</p>
+        </header>
+    );
+}
+
+export default Header;

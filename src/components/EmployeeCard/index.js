07f853abbaca66999f1f4ce00e2c71d90d91b86f
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-05-07 00:30:46 +0100

    added functionality for useState and useEffect

--- src/components/EmployeeCard/index.js
+++ src/components/EmployeeCard/index.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import API from "../../utils/API";
+import Employee from "../Employee";
 
 function EmployeeCard() {
+    // declare state variable for employees
+    const [employee, setEmployee] = useState({});
+    const [employees, setEmployees] = useState([]);
+
+    // when the component mounts, make call to get random employees
+    useEffect(() => {
+        loadEmployees();
+    }, []);
+
+    // declare function to load employees
+    // API call to fetch an employee
+    function loadEmployees() {
+        API.fetchEmployees()
+        .then(employees => {
+            setEmployees(employees);
+            setEmployee(employees[0]);
+        })
+
+        .catch(err => console.log(err));
+    }
+
     return (
         <div className="employee-card">
             <p>Hello, here is the list of employees!</p>
+            <Employee />
         </div>
     );
 }

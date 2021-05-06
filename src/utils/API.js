ca39e923e8f848991b74c1f4705e7f3c504e94ee
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-05-07 00:31:08 +0100

    added functionality for API call

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,7 +2,16 @@ import axios from "axios";
 
 export default {
     // declare function to get a random employee from the API source
-    getRandomUser: function () {
-        return axios.get("https://randomuser.me/api/");
+    fetchEmployees: function () {
+        return axios
+        .get("https://randomuser.me/api/")
+        .then(res => {
+            const employees = res.data;
+            return employees.map(employee => {
+                return {
+                    name: employee.name.first
+                }
+            })
+        })
     }
 }

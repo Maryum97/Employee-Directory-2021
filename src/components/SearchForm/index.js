e39708acc3ca44de96e0a0e13be58f808b557db4
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-05-07 14:08:40 +0100

    added search form component

---
+++ src/components/SearchForm/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function SearchForm() {
+    return (
+        <div className="search-form row g-2">
+
+            <div className="col-2">
+
+            </div>
+
+            <div className="col-8">
+                <input type="search" className="form-control" aria-describedby="search-help" placeholder="Search"></input>
+            </div>
+
+            <div className="col-2">
+
+            </div>
+
+        </div>
+    )
+}
+
+export default SearchForm;

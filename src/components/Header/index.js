f91ef875d2cc58c1c0e3aa6229e5742abe317717
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-05-07 14:20:39 +0100

    minor edits

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,7 @@ import "./style.css";
 function Header() {
     return (
         <header className="header">
-            <h1>Employee Tracker</h1>
+            <h1>Employee Directory</h1>
             <p>Search for an employee or sort by Name or Category:</p>
             <SearchForm />
         </header>

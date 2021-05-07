b2a5b28796f676614dcc6b444b60524528b82e65
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-05-07 13:40:37 +0100

    added headings for table columns

--- src/components/EmployeeCard/index.js
+++ src/components/EmployeeCard/index.js
@@ -1,6 +1,9 @@
+// main dependencies
 import React, { useState, useEffect } from "react";
 import API from "../../utils/API";
+// import components here
 import Employee from "../Employee";
+import TableColumm from "../TableColumn"
 
 function EmployeeCard() {
     // declare state variables
@@ -26,20 +29,33 @@ function EmployeeCard() {
 
     return (
         <div className="employee-card">
-            <p>Hello, here is the list of employees!</p>
-            {[...employees].map((employee => (
-                <Employee
-                    title={employee.title}
-                    firstName={employee.firstName}
-                    lastName={employee.lastName}
-                    age={employee.age}
-                    phone={employee.phone}
-                    email={employee.email}
-                    city={employee.city}
-                    country={employee.country}
-                    picture={employee.picture}
-                />
-            )))}
+            <TableColumm size="md-12">
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Photo</th>
+                            <th>Full Name</th>
+                            <th>Age</th>
+                            <th>Location</th>
+                            <th>Phone</th>
+                            <th>Email</th>
+                        </tr>
+                    </thead>
+                    {[...employees].map((employee => (
+                        <Employee
+                            title={employee.title}
+                            firstName={employee.firstName}
+                            lastName={employee.lastName}
+                            age={employee.age}
+                            phone={employee.phone}
+                            email={employee.email}
+                            city={employee.city}
+                            country={employee.country}
+                            picture={employee.picture}
+                        />
+                    )))}
+                </table>
+            </TableColumm>
         </div>
     );
 }

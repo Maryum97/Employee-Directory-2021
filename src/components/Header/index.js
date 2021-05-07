1f0f43aca1f17e2e2b916280094c2bc931639d19
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-05-07 14:09:04 +0100

    added component for search bar in the app

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,13 @@
 import React from "react";
+import SearchForm from "../SearchForm";
 import "./style.css";
 
 function Header() {
     return (
         <header className="header">
             <h1>Employee Tracker</h1>
-            <p>Search for an employee or sort by Name or Category</p>
+            <p>Search for an employee or sort by Name or Category:</p>
+            <SearchForm />
         </header>
     );
 }

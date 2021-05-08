40f4908af53ed2008fbae0d67504d4838e99aa2a
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-05-08 04:34:10 +0100

    edited buttpn styles

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./style.css";
 
 function SearchForm(props) {
     return (
         <div className="search-form row g-2">
 
-            <div className="col-7">
+            <div className="input col-7">
                 <input
                     type="search"
                     className="form-control"
@@ -18,20 +19,20 @@ function SearchForm(props) {
 
             </div>
 
-            <div className="col-2">
+            <div className="button col-2">
                 <button
                     type="submit"
-                    class="btn btn-primary"
+                    class="btn main-btn btn-secondary"
                     onClick={props.handleFormSubmit}
                 >
                     Find Employee
                 </button>
             </div>
 
-            <div className="col-2">
+            <div className="button col-2">
                 <button
                     type="submit"
-                    class="btn btn-danger"
+                    class="btn main-btn btn-danger"
                     onClick={props.clearSearch}
                 >
                     Reset Search
